91b8ac6e9a90fa0cc2d8f19ae266a594859ded64
vinmash <vinmash@mail.ru>
2023-08-23 20:42:05 +0500

    2

--- Main.java
+++ Main.java
@@ -1,9 +1,4 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
