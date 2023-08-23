c8d740669fafc6d323cc21a1ca2a105d7d727770
vinmash <vinmash@mail.ru>
2023-08-23 20:41:45 +0500

    2

--- Main.java
+++ Main.java
@@ -11,5 +11,10 @@ public class Main {
 //Код должен работать для строк любого размера и любого содержания.
 // Размер частей вы устанавливаете самостоятельно.
 
+        String s = "123456789123456sfsrdg yut78878789";
+        int size = 4;
+
+        String[] parts = s.split("(?<=\\G.{"+size+"})");
+        System.out.println(Arrays.toString(parts));
     }
 }
