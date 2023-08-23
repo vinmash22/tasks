f42589ad4a45e651f77b39f323d951c71b5bc6de
vinmash <vinmash@mail.ru>
2023-08-23 20:40:37 +0500

    1

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Main {
+    public static void main(String[] args) {
+//Напишите код, с помощью которого можно разделить строку на части.
+//Код должен работать для строк любого размера и любого содержания.
+// Размер частей вы устанавливаете самостоятельно.
+
+    }
+}
